4bc9eee9f967d6a37f105952d2ade8a0d6cb4728
viktorvirgo <110974722+viktorvirgo@users.noreply.github.com>
2022-09-11 19:18:50 +0300

    style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import './App.css';
 import { Item } from './elements/Item';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+
+const theme = createTheme({
+ 
+});
+
 
 function App() {
   return (
-    <div className='container'>
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className=''></div>
+      <Grid container spacing={0}>
+        <Grid item xs={4}>
+          <Card>
+            <Item>xs=4</Item>
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card>
+            <Item>xs=4</Item>
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card>
+            <Item>xs=4</Item>
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card>
+            <Item>xs=4</Item>
+          </Card>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+    
     
   )
    
